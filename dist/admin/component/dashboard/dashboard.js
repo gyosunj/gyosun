1ad8007f222725966c9c02a601299d3f374dabde
Gyosun Jung <gyosunjung@Gyosuns-Mac-Pro.T-mobile.com>
2017-02-05 13:06:58 -0800

    4.0

--- dist/admin/component/dashboard/dashboard.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-YeSeul.entree.dashboard = new YeSeul.entree();
-YeSeul.entree.dashboard.ctrl = function (route) {
-  var thisName = route.thisName;
-
-  var finalData;
-  YeSeul.dessert.mustache (route.renderTemplate, finalData, route.renderTarget)
-};
