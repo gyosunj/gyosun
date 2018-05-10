d1a47475c1053d8799df08ea6bd6231768ffea32
Gyosun Jung <gyosunj@gmail.com>
2018-05-09 21:06:57 -0700

    add load movies button

--- src/page/home/javascript/init.js
+++ src/page/home/javascript/init.js
@@ -1,4 +1,5 @@
 const plusModule = require('./plus');
+let movieCount = 0;
 
 module.exports = () => {
   const buttonEl = document.getElementsByClassName('component-button__button');
@@ -10,5 +11,25 @@ module.exports = () => {
   buttonEl[0].addEventListener('click', (event)=> {
     plusModule.incrementPrivateNumber();
     resultEl[0].innerText = plusModule.getPrivateNumber();
+  });
+
+  const moviesButtonEl = document.getElementsByClassName('component-movies-button__button');
+  const moviesResultEl = document.getElementsByClassName('component-movies-button__result');
+
+  moviesButtonEl[0].addEventListener('click', (event)=> {
+    window.fetch(`/api/movies/?sort=date&size=1&offset=${movieCount++}`)
+      .then((response) => response.json())
+      .then((json) => {
+        const movie = json.data[0];
+        const template = `
+          <div class="component-movies-button__result-movie-container" data-movie-id=${movie.id}>
+            <h4>${movie.title}</h4>
+            <div><b>Genre</b>: ${movie.genre}</div>
+            <div><b>Release Date</b>: ${movie.release_date}<div>
+          </div>
+        `;
+
+        moviesResultEl[0].insertAdjacentHTML('beforeend', template);
+      });
   });
 };
