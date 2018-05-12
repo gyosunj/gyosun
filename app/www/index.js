832376e346dfd4ce1cb53d4e7425d1b4ed427c9c
Gyosun Jung <gyosunj@gmail.com>
2018-05-11 22:37:40 -0700

    resolve conflict

--- app/www/index.js
+++ app/www/index.js
@@ -1,9 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   wwwHome: require('./home/'),
   wwwMarko: require('./marko-example'),
-=======
-  wwwHome: {method: 'GET', path: '/', handler: require('./home/index').index},
-  wwwMarko: {method: 'GET', path: '/marko', handler: require('./marko-example/index').index},
->>>>>>> fix minor bugs for marko
 };
